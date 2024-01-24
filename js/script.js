e15e147f191a52d96cf3258c78c0d77a56b9d623
priyanka shaw <priyankashaw680@gmail.com>
2024-01-24 22:55:40 +0530

    adding some changes

--- js/script.js
+++ js/script.js
@@ -16,16 +16,16 @@ let hr = timer.getHours();
 if(hr>=0 && hr<12){
     day_Night = "AM";
 }
-let h1 = document.createElement("h1");
+//let h1 = document.createElement("h1");
 let min = timer.getMinutes();
 let sec = timer.getSeconds();
 let msec = timer.getMilliseconds();
 console.log("hr", hr, "min" , min, "sec" , sec );
 let timeWtch = hr + ":" + min +":" + sec + day_Night ;
-document.querySelector(".clock").innerHTML = `<span>${hr} hour</span>
-                                              <span>${min} min</span>                        
-                                              <span>${sec} sec</span>
-                                              <span>${day_Night}</span>`;
+document.querySelector(".clock").innerHTML = `<span>${hr} :</span>
+                                              <span>${min} :</span>                        
+                                              <span>${sec}</span>
+                                              <span id= "dayNight">${day_Night}</span>`;
 
 
 
