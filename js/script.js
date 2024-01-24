e38e9baa9a34903e3a25994f62f1791552937da9
priyanka shaw <priyankashaw680@gmail.com>
2024-01-24 21:30:41 +0530

    adding file

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const mainContainer = document.querySelector("#timeContainer");
+const innerContainer = document.querySelector(".clock");
+//const hrs =  innerContainer.createElement("span");
+var day_Night = "PM";
+//element.classList.add("mystyle");
+var c =0;
+var s = 0;
+var m = 0;
+
+function time(){
+let timer = new Date();
+//console.log(timer);
+
+// let hr = timer.getHours();
+let hr = timer.getHours();
+if(hr>=0 && hr<12){
+    day_Night = "AM";
+}
+let h1 = document.createElement("h1");
+let min = timer.getMinutes();
+let sec = timer.getSeconds();
+let msec = timer.getMilliseconds();
+console.log("hr", hr, "min" , min, "sec" , sec );
+let timeWtch = hr + ":" + min +":" + sec + day_Night ;
+document.querySelector(".clock").innerHTML = `<span>${hr} hour</span>
+                                              <span>${min} min</span>                        
+                                              <span>${sec} sec</span>
+                                              <span>${day_Night}</span>`;
+
+
+
+}
+
+setInterval(time, 1000);
+
